ef723a6b80111d52c6da53916d7e55e5da226db3
neel-it-it <np209888@gmail.com>
2026-03-03 19:53:12 +0530

    Initial commit

---
+++ FinanceApp/Controllers/ExpensesController.cs
@@ -0,0 +1,42 @@
+﻿using FinanceApp.Data;
+using FinanceApp.Data.Service;
+using FinanceApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceApp.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        private readonly IExpensesService _expensesService;
+
+        public ExpensesController(IExpensesService expensesService)
+        {
+            _expensesService = expensesService;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var expenses =  await _expensesService.GetAll();
+            return View(expenses);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                await _expensesService.Add(expense);
+                return RedirectToAction("Index");
+            }
+            return View(expense);
+        }
+        public IActionResult GetChart()
+        {
+            var data = _expensesService.GetChartData();
+            return Json(data);
+        }
+    }
+}
---
+++ FinanceApp/Models/Expense.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinanceApp.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Description { get; set; } 
+        [Required]
+        [Range(0.01, double.MaxValue , ErrorMessage = "Amount should higher than 0")]
+        public double Amount { get; set; }
+        [Required]
+        public string? Category { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+    }
+}
